536e9bdfbafd3f9c7aa133b069a4f12040750315
Gabou <gabriel.weil@protonmail.com>
2018-12-10 21:27:29 +0100

    Les requêtes GET et GET /sequence fonctionnent. La requête POST ne fonctionne pas pour le moment car le client connecté au serveur n'a pas de token d'authentification. Par conséquent le serveur n'accepte pas de recevoir des données de ce client (https://stackoverflow.com/questions/50807260/enable-django-view-csfr-protection-for-anonymous-python-clients-using-api-reques).

--- Back_end/Animator/defaultView/urls.py
+++ Back_end/Animator/defaultView/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.defaultView_index, name='defaultView_index'),
+    path('sequence', views.sequence, name='sequence'),
 ]
--- Back_end/Animator/defaultView/views.py
+++ Back_end/Animator/defaultView/views.py
@@ -1,6 +1,20 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
+#from django.middleware.csrf import get_token
+
 
 def defaultView_index(request):
+    #csfr_token = get_token(request)
+    #if not request.session.session_key:
+    #    request.session.create()
+    #return JsonResponse({'token': csfr_token, 'sessionid' : request.session.session_key})
     return render(request, 'index.html')
+
+def sequence(request):
+    if request.method == "GET":
+        response_data = {}
+        return JsonResponse(response_data)
+    else:
+        return HttpResponse(status=200)

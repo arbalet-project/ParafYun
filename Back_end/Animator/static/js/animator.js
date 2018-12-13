4608d88e9e98edb7a9023bd3c7b634edddf18c36
Tourinel Florian <florian.tourinel@etu.u-bordeaux.fr>
2018-12-13 19:29:44 +0100

    modifications du front-end

--- Back_end/Animator/static/js/animator.js
+++ Back_end/Animator/static/js/animator.js
@@ -1,3 +1,49 @@
+var sliders = [];
 
-var slider = new Slider('#slider', {});
+var nbSliders;
 
+function affichageSliders() {
+  nbSliders = document.getElementById('nbSliders').value;
+  endroitOuAfficher = document.getElementById('affichageSliders');
+  sliders = [];
+
+  for (var i = 0; i < nbSliders; i++) {
+    var newSlide = document.createElement('input');
+
+    newSlide.id = "slider" + i;
+    newSlide.type = "text";
+    newSlide.class = "span2";
+    newSlide.value = "0";
+    newSlide['data-slider-min'] = "10";
+    newSlide['data-slider-max'] = "100";
+    newSlide['data-slider-step'] = "5";
+    newSlide['data-slider-value'] = "[250,450]";
+
+    endroitOuAfficher.appendChild(newSlide);
+
+    sliders.push(new Slider("#slider" + i, {orientation : 'vertical'}));
+
+    var br = document.createElement('br');
+
+    newSlide.appendChild(br);
+
+  }
+}
+
+function resetSliders(){
+
+}
+
+
+function enregistrer(){
+  var dicoContenu = [];
+  var stringBase = "#slider";
+
+  for(var i = 0; i < nbSliders; i++){
+    dicoContenu += {"nom" : stringBase + i, "type" : "open", "time" : 0.5};
+  }
+
+  var sequence = {"Sequence" : dicoContenu};
+
+  return sequence;
+}

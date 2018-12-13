b779145bc84be44f310ab95edc7a31f693fcd84b
Tourinel Florian <florian.tourinel@etu.u-bordeaux.fr>
2018-12-13 17:28:03 +0100

    correction de bugs

--- Back_end/Animator/static/js/animator.js
+++ Back_end/Animator/static/js/animator.js
@@ -1,31 +1,44 @@
 var sliders = [];
 
+var nbSliders;
 
 function affichageSliders() {
-  var nbSliders = document.getElementById('#nbSliders').value;
-  var insererAvant = document.getElementById('#insertBefore').value;
+  nbSliders = document.getElementById('nbSliders').value;
+  endroitOuAfficher = document.getElementById('affichageSliders');
+
 
   for (var i = 0; i < nbSliders; i++) {
-    var codeHtml = "<input id=\"slider" + i + " type=\"text\" class=\"span2\" value=\"\" data-slider-min=\"10\" data-slider-max=\"1000\" data-slider-step=\"5\" data-slider-value=\"[250,450]\"/>";
+    var newSlide = document.createElement('input');
 
-    insererAvant.insertBefore(codeHtml);
+    newSlide.id = 'slider' + i;
+    newSlide.type = 'text';
+    newSlide.class = 'span2';
+    newSlide.min = 10;
+    newSlide.max = 1000;
+    newSlide.value = [250,450];
 
-    sliders.push(new Slider("slider" + i));
+    endroitOuAfficher.appendChild(newSlide);
 
+  //  var codeHtml = " data-slider-step=\"5\" data-slider-value=\"[250,450]\"/>";
 
+    alert(i);
+
+  //  $( "affichageSliders" ).prepend( codeHtml );
+
+    // sliders.push(new Slider("#slider" + i, {}));
   }
 }
 
 
 function enregistrer(){
-  var dicoContenu;
+  var dicoContenu = [];
+  var stringBase = "#slider";
 
-  var stringBase = "slider";
   for(var i = 0; i < nbSliders; i++){
-    dicoContenu += {"nom" : alert(window[stringBase + i]), "type" : "open", "time" : 0.5};
+    dicoContenu += {"nom" : stringBase + i, "type" : "open", "time" : 0.5};
   }
 
-  var sequence = {"Sequence" : [dicoContenu]};
+  var sequence = {"Sequence" : dicoContenu};
 
   return sequence;
 }
--- programmer/README.md
+++
@@ -1,4 +0,0 @@
-# Paraf programmer
-
-The programmer is the user interface that allows to create new animations made of light and water droplets.
-It is a Python/Django-based server running the UI on client side.

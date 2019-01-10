a8d7ab20abd10dc7880f7086e1143a60129e5e15
Tourinel Florian <florian.tourinel@etu.u-bordeaux.fr>
2019-01-10 19:58:19 +0100

    requetes GET et POST depuis le front end fonctionelles (le POST pour sûr le GET pas testé)

--- Back_end/Animator/static/js/animator.js
+++ Back_end/Animator/static/js/animator.js
@@ -42,52 +42,77 @@ function resetSliders(){
 }
 
 function postData(){
-  jQuery(function($){
+  console.log("postData lancé");
+  console.log(localStorage.getItem('myStorage'));
 
+  jQuery(function($){
     jQuery.post("/sequence", JSON.parse(localStorage.getItem('myStorage')));
   });
 }
 
-
-function enregistrer(){
-  /*
-  var dicoContenu = [];
-  var stringBase = "#slider";
-
-  for(var i = 0; i < nbSliders; i++){
-    var valSliderActuel = document.getElementById("slider" + i).value;
-    dicoContenu += {"nom" : "stringBase" + i, "type" : "open", "time" : valSliderActuel};
-  }
-
-  var sequence = null;
-
-  jQuery(function($){
-      sequence = jQuery.parseJSON({"Sequence" : dicoContenu});
-  });
-
-  return sequence;
-
-  */
-
-   localStorage.setItem('myStorage', writeString())
+function saveSequence(){
+  localStorage.setItem('myStorage', writeString());
+  postData();
 }
 
 function writeString(){
- var sequence
+ var sequence = "{\"sequence\": [";
 
  for(var i = 0; i < nbSliders; i++){
-
    var valSliderActuel = document.getElementById("slider" + i).value;
-
-   if(valSliderActuel > 0.1 && i != nbSliders - 1){
-     sequence += '{ "name" : "valve"' + i + ', "type" : "open", time : "' + valSliderActuel + '" },';
-   }else if(valSliderActuel <= 0.1 && i != nbSliders -1){
-     sequence += '{ "name" : "valve"' + i + ', "type" : "closed", time : "' + valSliderActuel + '" },';
-   }else if(valSliderActuel > 0.1 && i == nbSliders - 1){
-     sequence += '{ "name" : "valve"' + i + ', "type" : "open", time : "' + valSliderActuel + '" }]';
-   }else if(valSliderActuel <= 0.1 && i == nbSliders -1){
-     sequence += '{ "name" : "valve"' + i + ', "type" : "closed", time : "' + valSliderActuel + '" }]';
+   var speed = calculTps(calculDist(valSliderActuel));
+   if(i != nbSliders - 1){
+     sequence += "{\"name\": \"valve" + i + "\", \"type\": \"open\", \"time\": \"" + speed + "\"}, ";
+   }else if(i != nbSliders -1){
+     sequence += "{\"name\": \"valve" + i + "\", \"type\": \"closed\", \"time\": \"" + speed + "\"}, ";
+   }else if(i == nbSliders - 1){
+     sequence += "{\"name\": \"valve" + i + "\", \"type\": \"open\", \"time\": \"" + speed + "\"}]}";
+   }else if(i == nbSliders -1){
+     sequence += "{\"name\": \"valve" + i + "\", \"type\": \"closed\", \"time\": \"" + speed + "\"}]}";
    }
  }
  return sequence;
 };
+
+function calculTps(dist){
+  return Math.sqrt((2*dist)/9.81);
+}
+
+function calculDist(valSliderActuel){
+  var emplacementVirgule = valSliderActuel.indexOf(',');
+  var premiereValeur = valSliderActuel.slice(0, emplacementVirgule);
+  var secondeValeur = valSliderActuel.slice(emplacementVirgule+1, valSliderActuel.lenght);
+  return secondeValeur - premiereValeur
+}
+
+function getData(){
+  var sequence = jQuery.parseJSON(document).ready(function(){
+    $.getJSON("/sequence");
+  });
+  readSequence(sequence);
+}
+
+function readSequence(sequence){
+  resetSliders();
+  var listEvent = sequence["sequence"];
+
+  for (var i = 0; i < listEvent.lenght; i++) {
+    var valeurs = listEvent[i];
+    var newSlide = document.createElement('input');
+
+    newSlide.id = "slider" + i;
+    newSlide.type = "text";
+    newSlide.class = "span2";
+
+    slider_options = {
+      orientation: "vertical",
+      min:10,
+      max:100,
+      step: 0.01,
+      value: [valeurs["time"]],
+      min: 0.0,
+      max: 2.0,
+      handle: "triangle"
+    }
+  }
+}
